ecb9f1ae439c0851d3a254bf6cbee57086b78ce0
514K <alekseysafronov0@gmail.com>
2023-03-28 18:59:54 +0300

    append differs

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var ANSI_RESET string = "\u001B[0m"
+var ANSI_RED string = "\u001b[48;5;210m"
+var ANSI_GREEN string = "\u001b[42m"
+var ANSI_REDL string = "\u001b[48;5;9m"
 
 func main() {
-	fmt.Printf("Hello, thats differ")
+	if len(os.Args) != 3 {
+		return
+	} else {
+		// fmt.Printf("We have 3 args\n")
+		file1, err1 := os.Open(os.Args[1])
+		file2, err2 := os.Open(os.Args[2])
+
+		if err1 != nil || err2 != nil {
+			fmt.Printf("Error: %v ; %v\n", err1.Error(), err2.Error())
+		} else {
+			defer file1.Close()
+			defer file2.Close()
+
+			data := make([]byte, 64)
+
+			// lines1 := make([]string, 64)
+			// lines2 := make([]string, 64)
+
+			// tmp := ""
+			for {
+				n, err := file1.Read(data)
+				if err == io.EOF {
+					break
+				}
+				fmt.Print(string(data[:n]))
+				break
+			}
+		}
+	}
+	fmt.Printf(ANSI_RED + "My red text?" + ANSI_REDL + "Sas" + ANSI_RED + "Ke" + ANSI_RESET)
 }
