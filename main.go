7721471ed9fec138e98e13a99878350b2602bb92
514K <alekseysafronov0@gmail.com>
2023-04-01 18:38:55 +0300

    add goroutines

--- main.go
+++ main.go
@@ -17,36 +17,54 @@ func main() {
 	if len(os.Args) != 3 {
 		return
 	} else {
-		// fmt.Printf("We have 3 args\n")
-		file1, err1 := os.Open(os.Args[1])
-		file2, err2 := os.Open(os.Args[2])
+		c1 := make(chan int)
+		arr1 := make([]string, 0)
 
-		if err1 != nil || err2 != nil {
-			fmt.Printf("Error: %v ; %v\n", err1.Error(), err2.Error())
-		} else {
-			defer file1.Close()
-			defer file2.Close()
+		c2 := make(chan int)
+		arr2 := make([]string, 0)
 
-			data := make([]byte, 1)
+		go readFile(os.Args[1], &arr1, c1)
 
-			// lines1 := make([]string, 64)
-			// lines2 := make([]string, 64)
+		go readFile(os.Args[2], &arr2, c2)
 
-			// tmp := ""
-			for {
-				n, err := file1.Read(data)
-				if err == io.EOF {
-					break
-				}
-
-				if n != 0 {
-
-				}
-				// fmt.Print(string(data[:n]))
+		for {
+			if <-c1 == 0 && <-c2 == 0 {
+				fmt.Printf("%v\n", arr1)
+				fmt.Printf("%v\n", arr2)
+				break
 			}
 		}
 	}
-	// fmt.Printf(ANSI_RED + "My red text?" + ANSI_REDL + "Sas" + ANSI_RED + "Ke" + ANSI_RESET)
-
+	// fmt.Printf(ANSI_RED + "My red text?" + ANSI_REDL + "Sas" + ANSI_RED + "Ke" + ANSI_RESET) ШАБЛОН КОДА ДЛЯ ВЫДЕЛЕНИЯ СТРОКИ В КОНСОЛИ
+	fmt.Print("\n")
 	fmt.Print(time.Since(t))
+}
+
+func readFile(filename string, arr *[]string, ch chan int) {
+
+	file, err := os.Open(filename)
+
+	if err == nil {
+		defer file.Close()
+
+		data := make([]byte, 1)
+
+		tmpstr := ""
+
+		for {
+			_, err := file.Read(data)
+			if err == io.EOF {
+				*arr = append(*arr, tmpstr+"\n")
+				break
+			}
+
+			if string(data) != "\n" {
+				tmpstr += string(data)
+			} else {
+				*arr = append(*arr, tmpstr+"\n")
+				tmpstr = ""
+			}
+		}
+	}
+	close(ch)
 }
